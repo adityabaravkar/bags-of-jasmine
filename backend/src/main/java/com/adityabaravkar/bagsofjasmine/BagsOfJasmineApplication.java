c1db0f0fd083c72ef1763c55a5658718410ffa2b
Aditya Baravkar <aditya.baravkar@sjsu.edu>
2023-04-16 17:59:28 -0700

    Initial project setup

---
+++ backend/src/main/java/com/adityabaravkar/bagsofjasmine/BagsOfJasmineApplication.java
@@ -0,0 +1,13 @@
+package com.adityabaravkar.bagsofjasmine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagsOfJasmineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagsOfJasmineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/adityabaravkar/bagsofjasmine/BagsOfJasmineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adityabaravkar.bagsofjasmine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagsOfJasmineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
